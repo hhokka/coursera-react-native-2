cee7c75d5d6e224b206939a0da8584431f1f0d08
Hans Hokka <hans.hokka@gmail.com>
2020-03-23 16:21:43 +0200

    Navigation 1

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { Card } from "react-native-elements";
+import { DISHES } from "../shared/dishes";
 
 function RenderDish(props) {
   const dish = props.dish;
