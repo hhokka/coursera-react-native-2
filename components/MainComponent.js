e2a9b38299ee0a59dcd152f94ce9105e14c82617
Hans Hokka <hans.hokka@gmail.com>
2020-03-24 13:21:44 +0200

    Icons #2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,8 +4,20 @@ import Menu from "./MenuComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import Dishdetail from "./DishdetailComponent";
-import { View } from "react-native";
-import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+import {
+  View,
+  Platform,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Text
+} from "react-native";
+import {
+  createStackNavigator,
+  createDrawerNavigator,
+  DrawerItems,
+  SafeAreaView
+} from "react-navigation";
 import { Icon } from "react-native-elements";
 
 const MenuNavigator = createStackNavigator(
@@ -114,6 +126,27 @@ const AboutNavigator = createStackNavigator(
   }
 );
 
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("./images/logo.png")}
+            style={styles.drawerImage}
+          />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
+        </View>
+      </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
 const MainNavigator = createDrawerNavigator(
   {
     Home: {
@@ -168,7 +201,8 @@ const MainNavigator = createDrawerNavigator(
     }
   },
   {
-    drawerBackgroundColor: "#D1C4E9"
+    drawerBackgroundColor: "#D1C4E9",
+    contentComponent: CustomDrawerContentComponent
   }
 );
 class Main extends Component {
@@ -185,5 +219,29 @@ class Main extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  drawerHeader: {
+    backgroundColor: "#512DA8",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row"
+  },
+  drawerHeaderText: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  drawerImage: {
+    margin: 10,
+    width: 80,
+    height: 60
+  }
+});
 
 export default Main;
