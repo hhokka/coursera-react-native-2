46fe3a5ecf2a621b0a5318730edc3ca1f1d389e6
Hans Hokka <hans.hokka@gmail.com>
2020-03-26 14:42:08 +0200

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from "./MenuComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import Dishdetail from "./DishdetailComponent";
+import Reservation from "./ReservationComponent";
 import {
   View,
   Platform,
@@ -97,7 +98,30 @@ const HomeNavigator = createStackNavigator(
     })
   }
 );
-
+const ReservationNavigator = createStackNavigator(
+  {
+    Reservation: { screen: Reservation }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff",
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={24}
+          iconStyle={{ color: "white" }}
+          onPress={() => navigation.navigate("DrawerToggle")}
+        />
+      )
+    })
+  }
+);
 const ContactNavigator = createStackNavigator(
   {
     Contact: { screen: Contact }
@@ -217,6 +241,21 @@ const MainNavigator = createDrawerNavigator(
             type="font-awesome"
             size={24}
             color={tintColor}
+          />
+        )
+      }
+    },
+    Reservation: {
+      screen: ReservationNavigator,
+      navigationOptions: {
+        title: "Reserve Table",
+        drawerLabel: "Reserve Table",
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name="cutlery"
+            type="font-awesome"
+            size={24}
+            iconStyle={{ color: tintColor }}
           />
         )
       }
